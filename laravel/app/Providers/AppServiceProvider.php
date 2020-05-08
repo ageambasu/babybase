e09b3a37a07e1c75ca5490d301b36b47c150acd6
Alberto Nieto <info@albertonieto.info>
2020-05-08 10:15:54 +0200

    fix MariaDB issues

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Fix MariaDB Specified key was too long error
+        Schema::defaultStringLength(191);
     }
 }
